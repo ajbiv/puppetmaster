498a89a36a79a17611be39b4528955d3f7f614b1
Alexander Bibighaus <alexander_b74@yahoo.com>
2012-08-02 15:44:19 +0000

    updating aws

--- manifests/aws.pp
+++ manifests/aws.pp
@@ -1,1 +1,9 @@
 package { "git" : ensure => installed }
+package { "git-core" : ensure => installed }
+package { "build-essential" : ensure => installed }
+package { "curl" : ensure => installed }
+#package { "ree" : ensure => installed }
+#package { "patch" : ensure => installed }
+#package { "zlib1g-dev" : ensure => installed }
+
+
